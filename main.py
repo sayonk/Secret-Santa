96b5abad28215712eda9daea44ec076b121601c6
Sayon Kathirgamanathan <sayon.mk@gmail.com>
2020-10-13 12:54:32 -0400

    Implemented seed method for random integers.

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ with open('SS-exclusions.txt', newline='') as f:
 participants = [i[0] for i in exclusions[1:]]
 chain = []
 
+# Initialize seed
+SEED = 2019
+
+# Creates a string of integers using the seed that will determine the order of future random integers
+random.seed(SEED)
+s = str(random.random())
+s_index = 2
+
 # Generates a chain until it is valid
 isValid = False
 while not isValid:
@@ -40,10 +48,29 @@ while not isValid:
             else:
                 # Generate a name until there is one that is not in the exclusions list
                 while rand in exclusions[last_index + 1]:
+
+                    # Generates a random integer using the next integer in the SEED string as the seed
+                    random.seed(int(s[s_index]))
+                    s_index += 1
                     rand = temp_participants[random.randint(0, len(temp_participants)) - 1]
 
+                    # Increase SEED by 1 to use a new string of integers when the previous one runs out
+                    if s_index == len(s):
+                        SEED += 1
+                        random.seed(SEED)
+                        s = str(random.random())
+                        s_index = 2
+
         else:
+            random.seed(int(s[s_index]))
+            s_index += 1
             rand = temp_participants[random.randint(0, len(temp_participants)) - 1]
+
+        if s_index == len(s):
+            SEED += 1
+            random.seed(SEED)
+            s = str(random.random())
+            s_index = 2
 
         # Add the valid name to the chain
         if isValid:
